0ad822676ca1c9964024a669d5c11cf7395bb71b
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2012-09-04 14:15:43 -0700

    Checking in changes prior to tagging of version 0.15.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 00decc7..8c258ba 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension Plack::Middleware::Session
    
    +0.15 Tue Sep  4 14:15:13 PDT 2012
    +    - Fixed CPAN dependencies
    +
     0.14 Tue Mar 29 13:48:42 PDT 2011
         - Support get_dbh callback in Store::DBI (kazeburo)

--- lib/Plack/Session/State/Cookie.pm
+++ lib/Plack/Session/State/Cookie.pm
@@ -2,7 +2,7 @@ package Plack::Session::State::Cookie;
 use strict;
 use warnings;
 
-our $VERSION   = '0.14';
+our $VERSION   = '0.15';
 our $AUTHORITY = 'cpan:STEVAN';
 
 use parent 'Plack::Session::State';
--- lib/Plack/Session/Store/Cache.pm
+++ lib/Plack/Session/Store/Cache.pm
@@ -2,7 +2,7 @@ package Plack::Session::Store::Cache;
 use strict;
 use warnings;
 
-our $VERSION   = '0.14';
+our $VERSION   = '0.15';
 our $AUTHORITY = 'cpan:STEVAN';
 
 use Scalar::Util qw[ blessed ];
